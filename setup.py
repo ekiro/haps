7cea2bed246847cb07df6455b516626df80492bc
Piotr Karkut <karkucik@gmail.com>
2017-11-23 15:48:16 +0100

    Update classifiers

--- setup.py
+++ setup.py
@@ -13,12 +13,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Operating System :: OS Independent",
         "Topic :: Software Development",
         "Intended Audience :: Developers",
